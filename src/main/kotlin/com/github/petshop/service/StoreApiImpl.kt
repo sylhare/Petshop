a87445da757fda3e53a43d9bc7355f2fd28f8c83
sylhare <sylhare@outlook.com>
2021-06-30 13:08:44 -0400

    Update StoreApiImpl

--- src/main/kotlin/com/github/petshop/service/StoreApiImpl.kt
+++ src/main/kotlin/com/github/petshop/service/StoreApiImpl.kt
@@ -2,21 +2,28 @@ package com.github.petshop.service
 
 import org.generated.petstore.api.StoreApi
 import org.generated.petstore.model.Order
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Does nothing, just as an example
+ */
+@RestController
 class StoreApiImpl : StoreApi {
-    override fun deleteOrder(orderId: String?) {
-        // do nothing
+    override fun deleteOrder(orderId: String?): ResponseEntity<Void>? {
+        return ResponseEntity.ok(null)
     }
 
-    override fun getInventory(): MutableMap<String, Int> {
-        return mutableMapOf()
+    override fun getInventory(): ResponseEntity<MutableMap<String, Int>>? {
+        return ResponseEntity.ok(null)
     }
 
-    override fun getOrderById(orderId: Long?): Order {
-        return Order()
+    override fun getOrderById(orderId: Long?): ResponseEntity<Order>? {
+        return ResponseEntity.ok(null)
     }
 
-    override fun placeOrder(order: Order?): Order {
-        return Order()
-    }
+// When not implemented, the default version in StoreApi will be used.
+//    override fun placeOrder(order: Order?): ResponseEntity<Order>? {
+//        return ResponseEntity.ok(null)
+//    }
 }
